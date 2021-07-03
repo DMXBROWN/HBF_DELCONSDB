e28fd5d318a027af622f187839afda54597f18d5
Dexter <Dexter@DEVPC15>
2021-07-03 12:10:24 +0400

    03-07-2021 Approval test

--- delconsdb_api/Controllers/UserController.cs
+++ delconsdb_api/Controllers/UserController.cs
@@ -14,12 +14,12 @@ namespace delconsdb_api.Controllers
     public class UserController : BaseController
     {
         private readonly IUserService _iuserservice;
-
+        
         public UserController(IUserService iuserservice)
         {
             _iuserservice = iuserservice;
         }
-
+        
         //GET api/User/Retrieve
         [AllowAnonymous]
         [HttpPost]       
@@ -28,13 +28,13 @@ namespace delconsdb_api.Controllers
             try
             {              
                 var result = _iuserservice.LogIn(user.User_Id, user.Passwd);
-
+                
                 if (result == null || result == String.Empty) 
                 {
                      return BadRequest(new { message = "User name or password is incorrect" });
                 }
                 
-
+                
                 return Ok(result);
             }
                   catch (Exception ex)
@@ -42,7 +42,7 @@ namespace delconsdb_api.Controllers
                       return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
                   }
         }
-
+        
         [AllowAnonymous]
         [HttpGet]
         public ActionResult<String> ProjectURL()
@@ -50,13 +50,13 @@ namespace delconsdb_api.Controllers
             try
             {
                 var result = _iuserservice.GetProjectURL();
-
+                
                 if (result == null || result == String.Empty)
                 {
                     return NotFound();
                 }
-
-
+                
+                
                 return Ok(result);
             }
             catch (Exception ex)
@@ -64,7 +64,7 @@ namespace delconsdb_api.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
-
+        
         [AllowAnonymous]
         [HttpGet]
         public ActionResult<String> ProductURL()
@@ -72,13 +72,13 @@ namespace delconsdb_api.Controllers
             try
             {
                 var result = _iuserservice.GetProductURL();
-
+                
                 if (result == null || result == String.Empty)
                 {
                     return NotFound();
                 }
-
-
+                
+                
                 return Ok(result);
             }
             catch (Exception ex)
@@ -86,7 +86,7 @@ namespace delconsdb_api.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
-
+        
         [AllowAnonymous]
         [HttpGet]
         public ActionResult<String> DownloadURL()
@@ -94,13 +94,13 @@ namespace delconsdb_api.Controllers
             try
             {
                 var result = _iuserservice.GetDownloadURL();
-
+                
                 if (result == null || result == String.Empty)
                 {
                     return NotFound();
                 }
-
-
+                
+                
                 return Ok(result);
             }
             catch (Exception ex)
@@ -108,8 +108,8 @@ namespace delconsdb_api.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
-
-
+        
+        
         [HttpGet]
         [Authorize(Roles = "admin,manager")]
         [ProducesResponseType(typeof(D_Gps_Interface), StatusCodes.Status200OK)]
@@ -118,15 +118,15 @@ namespace delconsdb_api.Controllers
         {
             
             var trackid = _iuserservice.GetTrackId();
-
+            
             if (trackid == null)
             {
                 return NotFound();
             }
-
+            
             return Ok(trackid);
         }
-
+        
         [HttpGet]
         [Authorize(Roles = "admin,manager")]
         [ProducesResponseType(typeof(UserDetails), StatusCodes.Status200OK)]
@@ -136,16 +136,16 @@ namespace delconsdb_api.Controllers
             var currentUser = HttpContext.User;
             string userid = currentUser.Identity.Name;
             var users = _iuserservice.GetUserDetails(userid);
-
+            
             if (users == null)
             {
                 return NotFound();
             }
-
+            
             return Ok(users);
         }
-
-
+        
+        
         [HttpPost]
         [Authorize(Roles = "admin,manager")]
         [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
@@ -155,15 +155,15 @@ namespace delconsdb_api.Controllers
             var currentUser = HttpContext.User;
             string userid = currentUser.Identity.Name;
             var users = _iuserservice.UploadPhoto(userid, file);
-
+            
             if (users == null)
             {
                 return "";
             }
-
+            
             return users;
         }
-
+        
         [HttpGet]
         [Authorize(Roles = "admin,manager")]
         [ProducesResponseType(typeof(UserProject), StatusCodes.Status200OK)]
@@ -173,15 +173,15 @@ namespace delconsdb_api.Controllers
             var currentUser = HttpContext.User;
             string userid = currentUser.Identity.Name;
             var proj = _iuserservice.GetUserProject(userid);
-
+            
             if (proj == null)
             {
                 return NotFound();
             }
-
+            
             return Ok(proj);
         }
-
+        
         [HttpGet]
         [Authorize(Roles = "admin,manager")]
         [ProducesResponseType(typeof(UserProduct), StatusCodes.Status200OK)]
@@ -191,14 +191,57 @@ namespace delconsdb_api.Controllers
             var currentUser = HttpContext.User;
             string userid = currentUser.Identity.Name;
             var proj = _iuserservice.GetUserProduct(userid, flag);
-
+            
             if (proj == null)
             {
                 return NotFound();
             }
-
+            
             return Ok(proj);
         }
-
+        
+        [HttpPost]
+        [Authorize(Roles = "admin,manager")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]         
+        public ActionResult<int> Approval()
+        {          
+            
+            int insertedRow;
+            
+            try
+            {
+                Approval_Test app_test = new Approval_Test();
+                DateTime dt = DateTime.Now;
+                app_test.Company_Code = 1;
+                app_test.Module = "PO";
+                app_test.Reportsys = "PO-T-211B";
+                app_test.Name = "Approval Email test";
+                app_test.Parameters ="Test Email";
+                app_test.Priority = 1;
+                app_test.Created_Date = dt;
+                app_test.Created_By = "AKA";
+                app_test.User_Id = "MAS";
+                app_test.Status = 0;
+                app_test.App_Key = "MAT_REQUISITION";
+                app_test.Subject = "Your request for approval is in process..";
+                
+                insertedRow = _iuserservice.InsertApproval(app_test);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            
+            if (insertedRow <= 0)
+            {
+                return Conflict();
+            }
+            else
+            {
+                return Ok(insertedRow);
+            }
+        }
+        
     }
 }
